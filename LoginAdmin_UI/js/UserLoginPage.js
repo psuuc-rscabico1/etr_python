ba2c560c865de8f973aa663d3170c6a5a32425ac
psuuc-rscabico1 <alitaptap2002@gmail.com>
2025-01-03 00:57:31 +0800

    "first"

--- LoginAdmin_UI/js/UserLoginPage.js
+++
@@ -1,6 +0,0 @@
-const loginSection = document.querySelector('.login-section');
-const loginLink = document.querySelector('.login-link');
-
-loginLink.addEventListener('click', () => {
-    loginSection.classList.remove('active');
-});
